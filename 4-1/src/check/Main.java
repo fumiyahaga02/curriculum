214ea73d3aa61d614d311336fdae164d4c7771d0
fumiyahaga02 <91002811+fumiyahaga02@users.noreply.github.com>
2021-10-11 10:39:23 +0900

    4-1
    4章課題訂正版

--- 4-1/src/check/Main.java
+++ 4-1/src/check/Main.java
@@ -4,19 +4,19 @@ import constants.Constants;
 
 public class Main {
 
+	private static String firstName = "Haga";
+	private static String lastName = "Fumiya";
+
 	public static void main(String[] args) {
 
 		System.out.println("printNameメソッド → " + printName(firstName, lastName));
-		
+
 		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 		pet.introduce();
 
 		RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		robotpet.introduce();
 	}
-
-	private static String firstName = "Haga";
-	private static String lastName = "Fumiya";
 
 	private static String printName(String firstName, String lastName) {
 		return firstName + lastName;

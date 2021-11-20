605225dbcaef09cb666d51056bb57eb7c689cccd
fumiyahaga02 <91002811+fumiyahaga02@users.noreply.github.com>
2021-11-20 18:10:43 +0900

    maxNumber

--- MaxNumber/src/Main.java
+++ MaxNumber/src/Main.java
@@ -4,27 +4,25 @@ public class Main {
 //	　　　　　1~Aの中で2で割れる回数が、一番多い数値を出力しなさい。
 //	　　　　　例）A=12
 //	         A/2 =6, 6/2=3, 二回
-		int A = 68;
-		int total = 0;
-		for (int i = 1; i <= A; i++) {
+//		訂正↓
+//		１～100の中で2で割れる回数をカウント
+//		→奇数なら条件分岐で奇数ですと表示
+
+		for (int i = 1; i <= 100; i++) {
 			int count = 0;
 			if (i % 2 == 1) {
+				System.out.println(i + "は奇数です");
 				continue;
 			}
 			for (int j = i; j % 2 == 0; j /= 2) {
 				count++;
 			}
-			if (count >= total) {
-				total = count;
-				System.out.println("一番多く割れた数→" + i);
-			}
+			System.out.println(i + "が2で割れる回数 " + count + "回");
 		}
-		System.out.println("");
-		System.out.println("一番多く割れる回数→" + total);
 	}
 }
 
-//1,2,3,...,67,68
+//1,2,3,...,100
 //64の場合
 //64 / 2 = 32
 //32 / 2 = 16

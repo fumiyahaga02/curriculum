dce277cffcfbf0ba21ac8b6785df7441bb9615fe
fumiyahaga02 <91002811+fumiyahaga02@users.noreply.github.com>
2021-11-22 15:09:00 +0900

    a

--- GetPrimeNumber/src/Main.java
+++ GetPrimeNumber/src/Main.java
@@ -1,30 +1,25 @@
+//解答
 
 public class Main {
-	public static int array[] = { 3, 7, 45, 1, 6, 75, 85, 24, 11, 87 };
+	public static int array[] = { 3, 7, 45, 1, 6, 75, 85, 24, 11, 87, 43 };
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 //		Question 素数を抽出するプログラムを作成しましょう。パラメータはint型の配列で入力されます。
-//		要素数は10。
-//		ではなく、
-//		１～100の中で2で割れる回数をカウント
-//		→２なら１回、６なら３回
-//		→奇数なら条件分岐で奇数ですと表示
-
-		for (int i = 1; i <= 100; i++) {
-			int count = 0;
-			if (i % 2 == 1) {
-				continue;
-			}
-			for (int j = i; j % 2 == 0; j /= 2) {
-				count++;
-			}
-			if (count >= total) {
-				total = count;
-				System.out.println("一番多く割れた数→" + i);
+//		要素数は入力者の任意です。
+		for (int i = 0; i < array.length; i++) {
+			// if (!変数名)は (変数 == false)のとき を表す。if文に書くのはboolean型と決まっているらしい
+			if (!judgePrime(i)) {
+				System.out.println(array[i]);
 			}
 		}
-		System.out.println("");
-		System.out.println("一番多く割れる回数→" + total);
+	}
+
+	public static boolean judgePrime(int i) {
+		for (int a = 2; a <= array[i] / 2; a++) {
+			if (array[i] % a == 0) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- MaxNumber/src/Main.java
+++ MaxNumber/src/Main.java
@@ -30,3 +30,22 @@ public class Main {
 //8 / 2 = 4
 //4 / 2 = 2
 //2 / 2 = 1
+
+//解答
+//public class Main {
+//public static void main(String[] args) {
+////	Question 1~100で偶数だけ取り出し、それぞれ何回２で割れるかを出力してください。
+//	int A=68;
+//	for(int i = 1;i<=A;i++) {
+//		if(i%2==0){
+//			int num=i;
+//			int count = 0;
+//			while(num%2==0) {
+//				num=num/2;
+//				count++;
+//			}
+//			System.out.println(i+"は2で"+count+"回割れます");
+//		}
+//	}
+//	}
+//}
